0286e72fb5cacbd46ed8ece600fad85336c2349d
raulmori <47406865+raulmori@users.noreply.github.com>
2022-01-23 18:40:23 -0500

    Update Ex1.cpp

--- Lec01/Ex1.cpp
+++ Lec01/Ex1.cpp
@@ -3,11 +3,8 @@
 #include <iostream> 
 
 // function main begins program execution 
-
 int main() 
-
 {
-
   std::cout << "Welcome to C++!\n";
 
   return 0; // indicate that program ended successfully 
